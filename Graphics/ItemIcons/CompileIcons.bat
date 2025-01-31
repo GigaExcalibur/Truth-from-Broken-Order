b14d0651470d9d02159fde23ae3af358a88f71b9
Dragz <jlevitt8962@gmail.com>
2025-01-30 16:34:00 -0800

    FINISH ITEM ICONS

--- Graphics/ItemIcons/CompileIcons.bat
+++ Graphics/ItemIcons/CompileIcons.bat
@@ -20,6 +20,42 @@
 @echo Axes generated!
 @del png.txt
 
+@cd "%~dp0\Bows"
+@dir *.png /b > png.txt
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m")
+@echo Bows generated!
+@del png.txt
+
+@cd "%~dp0\Anima"
+@dir *.png /b > png.txt
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m")
+@echo Anima generated!
+@del png.txt
+
+@cd "%~dp0\Light"
+@dir *.png /b > png.txt
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m")
+@echo Light generated!
+@del png.txt
+
+@cd "%~dp0\Dark"
+@dir *.png /b > png.txt
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m")
+@echo Dark generated!
+@del png.txt
+
+@cd "%~dp0\Staves"
+@dir *.png /b > png.txt
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m")
+@echo Staves generated!
+@del png.txt
+
+@cd "%~dp0\ItemsPrfs"
+@dir *.png /b > png.txt
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m")
+@echo Items and Prfs generated!
+@del png.txt
+
 echo Done!
 
 pause
